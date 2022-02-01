21fba0e7d1a8ef170687f5001f6663272abe52ed
Hein Ko Zin <heinkozin4@gmail.com>
2022-02-01 11:58:26 +0630

    Forgot page is added.

---
+++ pages/auth/forgot.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="w-full">
+    <Input
+      :data="forgot"
+      field="emailOrPassword"
+      type="text"
+      label="Enter email or phone number"
+      class="w-full"
+    />
+    <Button variant="primary" class="w-full mt-8" :disabled="!isFilledEmail">
+      Submit
+    </Button>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "AuthLayout",
+  data() {
+    return {
+      forgot: {
+        emailOrPassword: "",
+      },
+      isFilledEmail: false,
+      errors: {},
+    };
+  },
+
+  methods: {},
+
+  watch: {
+    verify: {
+      handler(newValue) {
+        const { emailOrPassword } = newValue;
+        if (emailOrPassword) {
+          this.isFilledEmail = true;
+        } else {
+          this.isFilledEmail = false;
+        }
+      },
+      deep: true,
+    },
+  },
+};
+</script>
+
+<style lang="postcss" scoped></style>
