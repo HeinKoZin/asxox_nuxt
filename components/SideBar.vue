5416449f80cc8b4894a6988210631e41549eb167
Hein Ko Zin <heinkozin4@gmail.com>
2022-02-04 21:04:38 +0630

    SideBar height issued is fixed on mobile. (Patch 1.0.1).

--- components/SideBar.vue
+++ components/SideBar.vue
@@ -68,7 +68,7 @@ export default {
 
 <style lang="postcss" scoped>
 .sidebar-container {
-  @apply w-[4.4rem] bg-slate-100 h-[calc(100%_-_4rem)] md:h-[calc(100vh_-_4rem)] p-1 py-4 flex  flex-col justify-between absolute md:relative z-50 transition-[margin];
+  @apply w-[4.4rem] bg-slate-100 h-full md:h-[calc(100vh_-_4rem)] p-1 py-4 flex  flex-col justify-between absolute md:relative z-50 transition-[margin];
 }
 
 .sidebar-container.slide-up {
