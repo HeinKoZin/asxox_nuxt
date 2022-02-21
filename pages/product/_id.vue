6c365c9f5c888826614b25385f573bf9559ba3e1
Hein Ko Zin <heinkozin4@gmail.com>
2022-02-21 13:23:31 +0630

    Recommended Products is added.

--- pages/product/_id.vue
+++ pages/product/_id.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="product-detail-container">
     <div class="product-cover">
-      <ProductCover />
+      <ProductCover ref="productCoverRef" />
     </div>
     <div class="product-detail">
       <ProductImages />
       <ProductDescription />
+      <RecommendedProducts />
+      <AdsShop />
     </div>
   </div>
 </template>
@@ -13,19 +15,39 @@
 <script>
 export default {
   layout: "MainLayout",
+
+  data() {
+    return {
+      //get height
+      height: 0,
+    };
+  },
+  computed: {
+    getHeight() {
+      return `h-[1050px]`;
+    },
+  },
+
+  mounted() {
+    this.height = this.$refs.productCoverRef.$el.clientHeight;
+  },
 };
 </script>
 
 <style lang="postcss" scoped>
 .product-detail-container {
-  @apply flex w-full md:flex-row flex-col h-full bg-slate-100 gap-2 md:gap-y-0 px-2;
+  @apply flex w-full md:flex-row flex-col bg-slate-100 gap-2 md:gap-y-0 px-2;
 }
 
 .product-cover {
-  @apply w-full md:w-1/5 min-h-[calc(100vh-4rem)];
+  @apply w-full h-full md:w-1/5  sticky top-0 bottom-0;
 }
 
 .product-detail {
-  @apply w-full md:w-4/5 flex gap-y-2 flex-col pb-4;
+  @apply w-full md:w-4/5 flex gap-y-2 flex-col;
+}
+
+.product-detail-container::-webkit-scrollbar {
+  @apply hidden;
 }
 </style>
