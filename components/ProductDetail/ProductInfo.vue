c98da4e32ebc19ed60e3a364c15c8f7ba2d0dbca
Hein Ko Zin <heinkozin4@gmail.com>
2022-02-22 11:39:39 +0630

    Product info design (Patch 1.0.1)

--- components/ProductDetail/ProductInfo.vue
+++ components/ProductDetail/ProductInfo.vue
@@ -2,8 +2,8 @@
   <div class="product-description-container">
     <div class="header">Product Information</div>
     <div class="body">
-      <div>
-        <span class="font-semibold">Brand: </span
+      <div class="brand">
+        <span>Brand:</span
         ><a href="#" class="font-bold text-blue-500 underline font-quicksand"
           >Asxox</a
         >
@@ -11,6 +11,11 @@
 
       <div class="product-code">
         <p>Product code: <span>#74324545</span></p>
+      </div>
+
+      <div class="availability-container">
+        <div class="availability-label">Availability:</div>
+        <div class="availability-value">In Stock</div>
       </div>
 
       <div class="categories-container">
@@ -44,6 +49,14 @@ export default {};
   @apply w-full p-2 flex flex-col gap-y-3;
 }
 
+.brand {
+  @apply flex w-full bg-slate-50 space-x-2;
+}
+
+.brand span {
+  @apply text-slate-800 font-bold text-base font-quicksand;
+}
+
 .categories-container {
   @apply flex flex-col gap-y-2 w-full font-quicksand font-semibold;
 }
@@ -57,10 +70,22 @@ export default {};
 }
 
 .product-code {
-  @apply text-slate-800 font-bold text-base font-quicksand w-full;
+  @apply text-slate-800 font-bold text-base font-quicksand w-full space-x-2;
 }
 
 .product-code span {
   @apply font-medium;
+}
+
+.availability-container {
+  @apply flex w-full gap-x-2 items-center font-quicksand flex-wrap;
+}
+
+.availability-container .availability-label {
+  @apply font-semibold;
+}
+
+.availability-container .availability-value {
+  @apply p-2  bg-green-600 text-white text-sm font-medium;
 }
 </style>
