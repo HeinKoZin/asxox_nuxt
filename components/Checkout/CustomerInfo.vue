c76f93f9e4327995328cb2b7d1c2f455afe2d8c8
Hein Ko Zin <heinkozin4@gmail.com>
2022-03-01 13:46:23 +0630

    CreateAddressModal is added.

--- components/Checkout/CustomerInfo.vue
+++ components/Checkout/CustomerInfo.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="customer-info-container">
-    <div class="w-full md:w-1/2 flex flex-col gap-y-2">
+    <div class="flex flex-col w-full md:w-1/2 gap-y-2">
       <CustomerDataCard />
       <PaymentMethodsCard />
     </div>
@@ -10,6 +10,8 @@
     <CustomerAddressCard />
 
     <OrderNoteCard />
+
+    <CreateAddressModal />
   </div>
 </template>
 
