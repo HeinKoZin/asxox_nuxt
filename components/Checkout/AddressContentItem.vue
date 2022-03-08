c885628d6f34448438c1e4def33ddd08bee1f2e4
Hein Ko Zin <heinkozin4@gmail.com>
2022-03-08 11:16:03 +0630

    add cursor pointer to address content item component

--- components/Checkout/AddressContentItem.vue
+++ components/Checkout/AddressContentItem.vue
@@ -69,7 +69,7 @@ export default {
 
 <style lang="postcss" scoped>
 .address-content-container {
-  @apply flex w-full bg-slate-100 p-4 px-6 items-center border-2 border-transparent gap-x-4 rounded-lg my-0 font-quicksand font-bold text-sm;
+  @apply flex w-full bg-slate-100 p-4 px-6 items-center border-2 border-transparent gap-x-4 rounded-lg my-0 font-quicksand font-bold text-sm cursor-pointer;
 }
 
 .address-content-checkbox {
