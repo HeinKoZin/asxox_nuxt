87ac653551eb8ac810c31d17c5546904000c6eb5
Hein Ko Zin <heinkozin4@gmail.com>
2022-03-11 11:19:32 +0630

    Wishlist is aded.

---
+++ components/User/Wishlist.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="wishlist-container">
+    <ProductCard
+      v-for="(data, index) in datas"
+      :key="index"
+      :data="data"
+      isInWishlist
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      //
+      datas: [
+        {
+          image:
+            "https://asxox-production-space.nyc3.digitaloceanspaces.com/upload/2022/01/29/products/feature/29-01-2022_Asxox_461f5034b333e30.38241242.jpg",
+          title: "Hair Scope Alabaster",
+          description: "This is a description of the product",
+          price: "15000 MMK",
+        },
+        {
+          image:
+            "https://asxox-production-space.nyc3.digitaloceanspaces.com/upload/2022/01/29/products/feature/29-01-2022_Asxox_461f5034b333e30.38241242.jpg",
+          title: "Hair Scope Alabaster",
+          description: "This is a description of the product",
+          price: "15000 MMK",
+        },
+        {
+          image:
+            "https://asxox-production-space.nyc3.digitaloceanspaces.com/upload/2022/01/29/products/feature/29-01-2022_Asxox_461f5023f807938.62020912.jpg",
+          title: "Elastic Fitness Stick",
+          description:
+            " This is a description of the product This is a description of the product This is a description of the product This is a description of the product This is a description of the product This is a description of the product",
+          price: "36000 MMK",
+        },
+        {
+          image:
+            "https://asxox-production-space.nyc3.digitaloceanspaces.com/upload/2022/01/29/products/feature/29-01-2022_Asxox_461f5034b333e30.38241242.jpg",
+          title: "Hair Scope Alabaster",
+          description: "This is a description of the product",
+          price: "15000 MMK",
+        },
+        {
+          image:
+            "https://asxox-production-space.nyc3.digitaloceanspaces.com/upload/2022/01/29/products/feature/29-01-2022_Asxox_461f5034b333e30.38241242.jpg",
+          title: "Hair Scope Alabaster",
+          description: "This is a description of the product",
+          price: "15000 MMK",
+        },
+        {
+          image:
+            "https://asxox-production-space.nyc3.digitaloceanspaces.com/upload/2022/01/29/products/feature/29-01-2022_Asxox_461f5034b333e30.38241242.jpg",
+          title: "Hair Scope Alabaster",
+          description: "This is a description of the product",
+          price: "15000 MMK",
+        },
+        {
+          image:
+            "https://asxox-production-space.nyc3.digitaloceanspaces.com/upload/2022/01/29/products/feature/29-01-2022_Asxox_461f5034b333e30.38241242.jpg",
+          title: "Hair Scope Alabaster",
+          description: "This is a description of the product",
+          price: "15000 MMK",
+        },
+
+        {
+          image:
+            "https://asxox-production-space.nyc3.digitaloceanspaces.com/upload/2022/01/29/products/feature/29-01-2022_Asxox_461f5034b333e30.38241242.jpg",
+          title: "Hair Scope Alabaster",
+          description: "This is a description of the product",
+          price: "15000 MMK",
+        },
+
+        {
+          image:
+            "https://asxox-production-space.nyc3.digitaloceanspaces.com/upload/2022/01/29/products/feature/29-01-2022_Asxox_461f5034b333e30.38241242.jpg",
+          title: "Hair Scope Alabaster",
+          description: "This is a description of the product",
+          price: "15000 MMK",
+        },
+
+        {
+          image:
+            "https://asxox-production-space.nyc3.digitaloceanspaces.com/upload/2022/01/29/products/feature/29-01-2022_Asxox_461f5034b333e30.38241242.jpg",
+          title: "Hair Scope Alabaster",
+          description: "This is a description of the product",
+          price: "15000 MMK",
+        },
+
+        {
+          image:
+            "https://asxox-production-space.nyc3.digitaloceanspaces.com/upload/2022/01/29/products/feature/29-01-2022_Asxox_461f5034b333e30.38241242.jpg",
+          title: "Hair Scope Alabaster",
+          description: "This is a description of the product",
+          price: "15000 MMK",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="postcss" scoped>
+.wishlist-container {
+  @apply w-full flex p-2 flex-wrap;
+}
+</style>
