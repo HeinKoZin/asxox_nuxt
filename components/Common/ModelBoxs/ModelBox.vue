8e4405a8f7deb8746108825027106b70c2af2a42
Hein Ko Zin <heinkozin4@gmail.com>
2022-03-25 19:03:53 +0630

    🖥️wip: ModelBox draft design

--- components/Common/ModelBoxs/ModelBox.vue
+++ components/Common/ModelBoxs/ModelBox.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="model-box-container-wrapper">
-    <div class="model-box-container">
+    <div class="model-box-container" :class="isLoader ? 'loader' : ''">
       <div class="model-box-icon">
         <i class="fa-solid fa-check" v-if="!isLoader"></i>
         <i class="fa-solid fa-circle-notch animate-spin" v-if="isLoader"></i>
       </div>
       <div class="model-box-content">
-        <h3>{{ content ? content : "Loading..." }}</h3>
+        <h3 class="w-full text-center">
+          {{ content ? content : "Loading..." }}
+        </h3>
+        <div class="model-action-buttons">
+          <button>Go to home</button>
+          <button>Go to orders</button>
+        </div>
       </div>
     </div>
   </div>
@@ -30,18 +36,30 @@ export default {
 
 <style lang="postcss" scoped>
 .model-box-container-wrapper {
-  @apply w-full h-full flex justify-center items-center fixed top-0 left-0 z-50;
+  @apply w-full h-full flex justify-center items-center fixed top-0 left-0 z-50 bg-slate-900 bg-opacity-10;
 }
 
 .model-box-container {
-  @apply p-4 px-10  flex flex-col items-center justify-center bg-orange-500 text-white rounded-lg border border-slate-300;
+  @apply p-4 px-10  flex flex-col items-center justify-center  text-white rounded-lg border border-slate-300;
 }
 
-.model-box-content {
+.model-box-container.loader {
+  @apply border-0;
+}
+
+.model-box-container.loader .model-box-content {
   @apply font-quicksand text-sm md:text-base lg:text-lg py-2;
 }
 
 .model-box-icon {
   @apply text-lg md:text-xl lg:text-2xl;
+}
+
+.model-action-buttons {
+  @apply flex  items-center justify-center gap-2;
+}
+
+.model-action-buttons button {
+  @apply flex-grow p-2 rounded-full bg-white text-slate-500 hover:bg-slate-700 hover:text-white;
 }
 </style>
