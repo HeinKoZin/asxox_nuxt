03b5093216b393c0fa6963922075a12015fb8859
Hein Ko Zin <heinkozin4@gmail.com>
2022-04-18 17:03:24 +0630

    Test

---
+++ pages/user/points.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="points-container">
+    <div class="points-detail-container">
+      <div class="points-item">
+        <h2>My Points</h2>
+        <p>
+          You have <span class="points">{{ points }}</span> points.
+        </p>
+      </div>
+      <div class="points-item">
+        <h2>My Points</h2>
+        <p>
+          You have <span class="points">{{ points }}</span> points.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "ProfileLayout",
+
+  data() {
+    return {
+      points: 0,
+    };
+  },
+};
+</script>
+
+<style lang="postcss" scoped>
+.points-container {
+  @apply flex w-full bg-slate-100 lg:p-4 p-1 rounded-lg flex-col;
+}
+
+.points-detail-container {
+  @apply flex  items-center gap-2;
+}
+
+.points-item {
+  @apply bg-slate-100 w-1/2 font-quicksand font-bold text-center p-4 py-6 border-2 border-slate-300 rounded-md;
+}
+</style>
