829a31b83359df6f825e12b8069e9d7b84a52ceb
Hein Ko Zin <heinkozin4@gmail.com>
2022-04-19 16:56:27 +0630

    🖥️wip: point page is designed

--- pages/user/points.vue
+++ pages/user/points.vue
@@ -7,18 +7,18 @@
           You have <span class="points">{{ points }}</span> points.
         </p>
       </div>
-      <div class="points-item">
-        <h2>My Points</h2>
-        <p>
-          You have <span class="points">{{ points }}</span> points.
-        </p>
+      <div class="point-action-buttons">
+        <button>Top up</button>
+        <button>Transfer</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Button from "../../components/Common/Button.vue";
 export default {
+  components: { Button },
   layout: "ProfileLayout",
 
   data() {
@@ -31,14 +31,26 @@ export default {
 
 <style lang="postcss" scoped>
 .points-container {
-  @apply flex w-full bg-slate-100 lg:p-4 p-1 rounded-lg flex-col;
+  @apply flex w-full h-full bg-slate-100 lg:p-4 p-1 py-6 rounded-lg flex-col;
 }
 
 .points-detail-container {
-  @apply flex  items-center gap-2;
+  @apply flex  items-center gap-4 flex-col justify-center h-full w-full;
 }
 
 .points-item {
-  @apply bg-slate-100 w-1/2 font-quicksand font-bold text-center p-4 py-6 border-2 border-slate-300 rounded-md;
+  @apply bg-slate-100 w-1/2 mx-auto md:mx-0 font-quicksand font-bold text-center p-4 py-6 border-2 border-slate-300 rounded-md;
+}
+
+.points-item h2 {
+  @apply text-2xl text-slate-600 mb-2;
+}
+
+.point-action-buttons {
+  @apply flex  w-1/2  gap-4;
+}
+
+.point-action-buttons button {
+  @apply bg-orange-500 w-full py-3 px-4 text-center text-sm lg:text-lg  text-white font-quicksand font-semibold rounded-md;
 }
 </style>
