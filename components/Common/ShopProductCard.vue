cc25a170dcffbb682d1a62070d13a43c4fd215bd
Hein Ko Zin <heinkozin4@gmail.com>
2022-04-30 16:51:57 +0630

    🖥️wip: product card background from img

--- components/Common/ShopProductCard.vue
+++ components/Common/ShopProductCard.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="shop-product-card-container-wrapper">
-    <div class="shop-product-card-container">
+    <div class="shop-product-card-container" ref="productContainer">
       <div class="shop-product-card-image-container">
         <img
           :src="product.temp_photo"
           alt=""
           srcset=""
+          ref="productCover"
           class="shop-product-card-image"
+          crossorigin="anonymous"
         />
       </div>
       <div class="product-details-container">
-        <h3>{{ product.name }}</h3>
+        <h3>{{ product.name + "." + palette }}</h3>
         <p>{{ product.sell_price + " " + product.currency }}</p>
       </div>
     </div>
@@ -18,11 +20,78 @@
 </template>
 
 <script>
+import ColorThief from "colorthief";
+
 export default {
   props: {
     product: {
       type: Object,
     },
+  },
+
+  data() {
+    return {
+      bgColor: "",
+    };
+  },
+
+  computed: {
+    backgroundColor() {
+      return `bg-[${this.bgColor}]`;
+    },
+  },
+
+  methods: {
+    //  get hex color from rgb
+    getHexColor(r, g, b) {
+      return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+    },
+
+    async getPalette() {
+      // run on server
+
+      if (process.server) {
+        const colorThief = new ColorThief();
+        // const img = document.querySelector("img");
+
+        // Make sure image is finished loading
+        if (img.complete) {
+          const color = colorThief.getColor(this.product.temp_photo);
+          this.bgColor = this.getHexColor(color[0], color[1], color[2]);
+          // apply this color to ref productCover
+          this.$refs.productContainer.style.backgroundColor = this.bgColor;
+        } else {
+          image.addEventListener("load", function () {
+            const color = colorThief.getColor(this.product.temp_photo);
+            this.bgColor = this.getHexColor(color[0], color[1], color[2]);
+            this.$refs.productContainer.style.backgroundColor = this.bgColor;
+          });
+        }
+        // const img =
+        //   "https://asxox-production-space.nyc3.digitaloceanspaces.com/upload/2022/04/26/products/feature/26-04-2022_Asxox_46267b31ccebee2.51432099.jpg";
+        // colorThief
+        //   .getColor(img)
+        //   .then((color) => {
+        //     console.log(color);
+        //   })
+        //   .catch((err) => {
+        //     console.log(err);
+        //   });
+
+        // colorThief
+        //   .getPalette(img, 5)
+        //   .then((palette) => {
+        //     console.log(palette);
+        //   })
+        //   .catch((err) => {
+        //     console.log(err);
+        //   });
+      }
+    },
+  },
+
+  mounted() {
+    this.getPalette();
   },
 };
 </script>
@@ -33,15 +102,15 @@ export default {
 }
 
 .shop-product-card-container {
-  @apply flex bg-slate-200 text-black p-2 rounded-xl shadow-sm;
+  @apply flex text-slate-100 rounded-xl shadow-sm flex-col justify-center items-center;
 }
 
 .shop-product-card-image-container {
-  @apply h-auto w-20 md:w-44;
+  @apply h-auto w-full;
 }
 
 .shop-product-card-image {
-  @apply object-cover w-full h-full rounded-xl;
+  @apply object-cover w-full h-full rounded-t-xl;
 }
 
 .product-details-container {
